61f94df50750b24fa826b700f24aca96ab0e074d
Louis-Marin MATHOREL <mathorel_louis@hotmail.fr>
2020-02-23 08:25:18 +0100

    code : tâche 1 - suppression saisie directe des kilomètres

--- app/src/main/java/fr/cned/emdsgil/suividevosfrais/KmActivity.java
+++ app/src/main/java/fr/cned/emdsgil/suividevosfrais/KmActivity.java
@@ -1,9 +1,8 @@
 package fr.cned.emdsgil.suividevosfrais;
 
 import android.os.Bundle;
-
+//import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.DatePicker.OnDateChangedListener;
@@ -12,8 +11,8 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -70,56 +69,56 @@ public class KmActivity extends AppCompatActivity {
         if (Global.listFraisMois.containsKey(key)) {
             qte = Global.listFraisMois.get(key).getKm();
         }
-        ((EditText) findViewById(R.id.txtKm)).setText(String.format(Locale.FRANCE, "%d", qte));
+        ((TextView) findViewById(R.id.txtNuitees)).setText(String.format(Locale.FRANCE, "%d", qte));
     }
 
     /**
      * Sur la selection de l'image : retour au menu principal
      */
     private void imgReturn_clic() {
-        findViewById(R.id.imgKmReturn).setOnClickListener(new ImageView.OnClickListener() {
+        findViewById(R.id.imgNuiteesReturn).setOnClickListener(new ImageView.OnClickListener() {
             public void onClick(View v) {
                 retourActivityPrincipale();
             }
-        });
+        }) ;
     }
 
     /**
      * Sur le clic du bouton valider : sérialisation
      */
     private void cmdValider_clic() {
-        findViewById(R.id.cmdKmValider).setOnClickListener(new Button.OnClickListener() {
+        findViewById(R.id.cmdNuiteeValider).setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 Serializer.serialize(Global.listFraisMois, KmActivity.this);
                 retourActivityPrincipale();
             }
-        });
+        }) ;
     }
-
+    
     /**
      * Sur le clic du bouton plus : ajout de 10 dans la quantité
      */
     private void cmdPlus_clic() {
-        findViewById(R.id.cmdKmPlus).setOnClickListener(new Button.OnClickListener() {
+        findViewById(R.id.cmdNuiteePlus).setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 qte += 10;
                 enregNewQte();
             }
-        });
+        }) ;
     }
-
+    
     /**
      * Sur le clic du bouton moins : enlève 10 dans la quantité si c'est possible
      */
     private void cmdMoins_clic() {
-        findViewById(R.id.cmdKmMoins).setOnClickListener(new Button.OnClickListener() {
+        findViewById(R.id.cmdNuiteeMoins).setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 qte = Math.max(0, qte - 10); // suppression de 10 si possible
                 enregNewQte();
             }
-        });
+        }) ;
     }
-
+    
     /**
      * Sur le changement de date : mise à jour de l'affichage de la qte
      */
@@ -138,7 +137,7 @@ public class KmActivity extends AppCompatActivity {
      */
     private void enregNewQte() {
         // enregistrement dans la zone de texte
-        ((EditText) findViewById(R.id.txtKm)).setText(String.format(Locale.FRANCE, "%d", qte));
+        ((TextView) findViewById(R.id.txtNuitees)).setText(String.format(Locale.FRANCE, "%d", qte));
         // enregistrement dans la liste
         Integer key = annee * 100 + mois;
         if (!Global.listFraisMois.containsKey(key)) {
